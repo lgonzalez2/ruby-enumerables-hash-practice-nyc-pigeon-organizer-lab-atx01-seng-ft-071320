ea2f119c126596e2ff26991052c34b63e8ef93e8
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-06 22:51:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,10 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   
+  
+  
+  binding.pry
 end
 
 
 
-
-
-
-# What I'm thinking rn is to go through the hashes and collect all the names, puting them into a new hash first and the going from there. If it's a new hash, it will work with other data sets
