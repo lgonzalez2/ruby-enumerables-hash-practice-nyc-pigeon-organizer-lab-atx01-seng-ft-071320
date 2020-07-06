179fd1c9876f14cc8c1d9b86fc1087edd7acd21d
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-06 23:11:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,7 +15,7 @@ def nyc_pigeon_organizer(data)
           final_array[name][key] = []
         end 
         
-        final_array[name][key].push
+        final_array[name][key].push(key2)
         
       end 
       
