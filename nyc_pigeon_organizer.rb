1d2c106a8f21783745284f5a0bbaa8a3e513760d
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-06 23:15:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  data.each_with_object({}) do |(key, value), final_array|
+    value.each do |key2, names|
+      names.each do |name|
+        
+        if !final_array[name]
+          final_array[name] = {}
+        end 
+        
+        if !final_array[name][key]
+          final_array[name][key] = []
+        end 
+        
+        final_array[name][key].push(key2.to_s)
+      end 
+    end 
+  end
 end
+
