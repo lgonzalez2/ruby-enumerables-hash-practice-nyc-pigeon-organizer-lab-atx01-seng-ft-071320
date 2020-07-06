da40c4646aed2d105746fbe2218a2bffe772929d
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-06 22:58:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,10 @@ def nyc_pigeon_organizer(data)
   
   data.each_with_object({}) do |(key, value), final_array|
     
-    binding.pry
+    
+    
     final_array
     
-    binding.pry
   end
 end
 
