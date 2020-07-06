fdc082dc257f713bae26c42127e8a4a85333d8d7
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-06 23:15:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,26 +3,20 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   
   data.each_with_object({}) do |(key, value), final_array|
-    
     value.each do |key2, names|
-      
       names.each do |name|
         
         if !final_array[name]
           final_array[name] = {}
         end 
+        
         if !final_array[name][key]
           final_array[name][key] = []
         end 
         
         final_array[name][key].push(key2.to_s)
-        
       end 
-      
     end 
-    
-    
-    final_array
   end
 end
 
